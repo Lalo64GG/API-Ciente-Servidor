e56ad4df690ae7f2c9f2fc4c1646d26732a57bf5
Lalo64GG <223257@ids.upchiapas.edu.mx>
2024-01-21 22:18:52 -0800

    Colecciones agregadas

--- src/Routes/Menssage.route.js
+++ src/Routes/Menssage.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const messageController = require('../Controllers/message.controller');
 
 router.get('/:senderId', messageController.getMessages);
-router.get('/nuevo', messageController.getNuevoMessage);
+router.get('/message/nuevo', messageController.getNuevoMessage);
 router.post('/new', messageController.createMessage);
 
 module.exports = router;
